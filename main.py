cfc3965a6c382678feb61680bd331f22c051de44
Emerson Smith <psyes11@nottingham.ac.uk>
2024-06-08 11:08:33 +0100

    changing back to input() and add print max x

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-#max_x = input('What is the max x?')
-max_x = os.getenv('MAX_X')
+max_x = input('What is the max x?')
+print(max_x)
+#max_x = os.getenv('MAX_X')
 print()
 for x in range(0, int(max_x)):
     print(x)
